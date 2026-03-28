0fb179907afb5261fea3530d69bde91a70e4565e
Alberto Pilotto <a.pilotto@icloud.com>
2026-03-28 20:13:46 -0300

    Blender `helper files` for IDE syntax and linting

---
+++ cpp_rna_runtime.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/* IntelliSense-only overrides for Blender RNA runtime view.
+ * This file is NOT used by the actual build, only by VS Code.
+ */
+
+#ifndef RNA_RUNTIME
+#  define RNA_RUNTIME
+#endif
